fd7afe2b020f4ed04dd7f06894f8cbe30bb459fd
anlque <an.cr.0999@gmail.com>
2025-12-22 17:31:24 +0400

    remove loki screens, upd server, fly toml

--- json-server/index.js
+++ json-server/index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
 const https = require('https');
-const http = require('http');
 
 const options = {
     key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
@@ -59,16 +58,10 @@ server.use((req, res, next) => {
 server.use(router);
 
 // Run server
-const PORT = 8443;
-const HTTP_PORT = 8000;
+const PORT = process.env.PORT || 8080;
 
 const httpsServer = https.createServer(options, server);
-const httpServer = http.createServer(server);
 
 httpsServer.listen(PORT, () => {
     console.log(`server is running on ${PORT} port`);
-});
-
-httpServer.listen(HTTP_PORT, () => {
-    console.log(`server is running on ${HTTP_PORT} port`);
 });
